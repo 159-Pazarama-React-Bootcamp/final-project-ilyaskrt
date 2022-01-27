84c57955886bd8699b90bac72c6b540975efaee9
İlyas <ilyaskrt19@gmail.com>
2022-01-27 15:54:46 +0300

    created home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import Auth from "./auth"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to relofad.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Routes>
+        <Route path="basvuru-olustur" element={<Home />} />
+        <Route path="basvuru-basarili" element={<Successful/>} />
+        <Route path="basvuru-sorgula" element={<Query/>} />
+      </Routes>
     </div>
   );
 }
 
+function Home() {
+  return (
+    <>
+      <main>
+        <h2 >Başvuru Formunu</h2>
+        <Auth></Auth>
+      </main>
+      <nav>
+        <Link to="/basvuru-sorgula">Başvuru Durumu Sorgula</Link>
+      </nav>
+    </>
+  );
+}
+
+function Successful(){
+  return(
+    <>
+    <nav>
+      <Link to="/basvuru-olustur">Basvuru ekranına git.</Link> 
+    </nav>
+    </>
+  );
+}
+function Query(){
+    return(
+    <>
+    <main>
+        <p>Sorgulama ekranına hoş geldiniz.</p>
+    </main>
+    <nav>
+      <Link to="/basvuru-olustur">Basvuru ekranına git.</Link> 
+    </nav>
+    </>
+    );
+}
 export default App;
